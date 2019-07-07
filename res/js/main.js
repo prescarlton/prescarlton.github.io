e554113d876d5dcdfec3466622af83e2f9bda196
Preston <prescarlton@gmail.com>
2019-07-07 12:01:03 -0400

    finished about section

---
+++ res/js/main.js
@@ -0,0 +1,13 @@
+
+function setColors() {
+    //sets the accent colors of the webpage randomly
+    //todo: make this work
+}
+
+window.addEventListener('load', function () {
+    setColors();
+    
+    //logic to make navbar change color when scrolling
+
+});
+
